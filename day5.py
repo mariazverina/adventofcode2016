609523de3f9f9db9c86d306247d4ef51433f3bb6
Maria Zverina <emz@fb.com>
2016-12-10 23:47:32 +0000

    Day 5 done

---
+++ day5.py
@@ -0,0 +1,26 @@
+import hashlib
+
+door = "cxdnnyjw"
+
+password = [None] * 8
+
+print password
+
+cnt = 0
+
+while True:
+    m = hashlib.md5()
+    m.update(door + str(cnt))
+    h = m.hexdigest()
+    if h[:5] == "00000":
+        pos = int(h[5], 16)
+        if pos < len(password) and password[pos] is None:
+            password[pos] = h[6]
+        if not None in password:
+            break
+        print h, cnt, password
+
+    cnt += 1
+
+print ''.join(password)
+
