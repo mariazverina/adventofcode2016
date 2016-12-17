2dadf1b5dde1e551a9120f36ee227d56454376fe
Maria Zverina <emz@fb.com>
2016-12-17 13:15:57 +0000

    Switching to python3

--- day16.py
+++ day16.py
@@ -1,25 +1,27 @@
 import operator
 
 def dragon(x):
-    return x + [False] + map(operator.not_, input)[::-1]
+    return x + [False] + [*map(operator.not_, input)][::-1]
 
 def decode(x):
     return ''.join(map(lambda q:str(int(q)), x))
 
 input = "00101000101111010"
-input = map(lambda x:x=='1', input)
+input = [*map(lambda x:x=='1', list(input))]
 
 req_len = 35651584
 
+print(input)
+
 while len(input) < req_len:
     input = dragon(input)
-    print len(input)
+    print(len(input))
     # print decode(input)
 
 input = input[:req_len]
 
 while len(input) % 2 == 0:
     input = [a == b for a,b in zip(*([iter(input)]*2))]
-    print len(input)
+    print(len(input))
 
-print decode(input)
+print(decode(input))
