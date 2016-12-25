7f5b5909ff72e209553ca4be528a48d9d8fd748d
Maria Zverina <emz@fb.com>
2016-12-25 12:42:28 +0000

    Day 24 done

--- day24.py
+++ day24.py
@@ -1,31 +1,66 @@
+from itertools import permutations
+
 with open(__file__[:-2]+"txt", "r") as f:
     maze = list(map(str.strip, f.readlines()))
 
-print(maze)
+# maze = """###########
+# #0.1.....2#
+# #.#######.#
+# #4.......3#
+# ###########""".split()
+#
 
-print([x for x in ''.join(maze) if x not in ".#"])
-print(len(maze), len(maze[0]))
-
-
-paths = [(0,0, code)]
-minpath = maxpath = None
+print(len(maze[0]), "x", len(maze))
 
 height = len(maze)
-while len(paths):
-    x, y, path = paths.pop(0)
-    if x < 0 or x > 3 or y < 0 or y > 3:
-        continue
-    if x == 3 and y == 3:
-        if not minpath:
-            minpath = path
-        maxpath = path
-        continue
-    doors = open_doors(path)
-    for d in DIRS:
-        if doors[d]:
-            xd, yd = OFFSETS[d]
-            paths.append((x+xd, y+yd, path+d))
+width = len(maze[0])
+locations = {maze[y][x]:(x,y) for y in range(height) for x in range(width) if maze[y][x] not in ".#"}
+print("locations", locations)
 
-print("min", minpath)
-print("max", len(maxpath[8:]), maxpath[8:])
+mindists = {}
+
+for source in locations.keys():
+    mindists[source] = {}
+    paths = [tuple(list(locations[source]) + [0])]
+    visited = set()
+    while len(mindists[source]) < len(locations) and len(paths):
+        x, y, dist = paths.pop(0)
+        if (x, y) in visited:
+            continue
+        visited.add((x,y))
+        if x < 0 or x >= width or y < 0 or y >= height:
+            continue
+        cell = maze[y][x]
+        if cell == '#':
+            continue
+        if cell != '.':
+            # we found a target
+            if cell not in mindists[source]:
+                mindists[source][cell] = dist
+
+        for a,b in [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]:
+            if not (a,b) in visited:
+                paths.append((a,b,dist+1))
+
+
+print(mindists)
+
+dests = list(locations.keys())
+dests.remove('0')
+
+def eval_path(path):
+    global mindists
+    src = '0'
+    d = 0
+    for n in path:
+        d += mindists[src][n]
+        src = n
+    return (d, path)
+
+paths = list(permutations(dests))
+
+# part 1
+print(sorted(map(eval_path, paths))[:5])
+# part 2
+print(sorted(map(lambda x:eval_path(list(x)+['0']), paths))[:5])
 
